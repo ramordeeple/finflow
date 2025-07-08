25ebc98e270c093d4e52e76d68bc39b7b86336bd
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-07-09 02:34:58 +0300

    handling starting port and GetCashflowHandler method in main.go

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	db := InitDB()
@@ -8,7 +12,16 @@ func main() {
 
 	http.HandleFunc("/v1/cashflow", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			
+			GetCashflowHandler(db, w, r)
+		} else {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		}
 	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Println("Server started on port ", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
